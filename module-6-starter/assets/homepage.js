933ea01a0d5c1f11077223feb0b4bc08d53bd625
Sabriel Gee <sabrielgee@sabriels-imac.lan>
2021-11-26 13:31:46 -0700

    redirect and url query

--- module-6-starter/assets/homepage.js
+++ module-6-starter/assets/homepage.js
@@ -9,6 +9,7 @@ var formSubmitHandler = function(event) {
     var username = nameInputEl.value.trim();
     if (username) {
         getUserRepos(username);
+        repoContainerEl.textContent = "";
         nameInputEl.value = "";
     } else {
         alert("Please enter a GitHub username");
@@ -23,11 +24,11 @@ var getUserRepos = function(user) {
         console.log(response);
         if (response.ok) {
         response.json().then(function(data) {
-            displayRepos(data, user);
             console.log(data);
+            displayRepos(data, user);
         });
     } else {
-        alert("Error: GitHub User Not Found");
+        alert("Error: " + response.statusText);
     }
     })
     .catch(function(error) {
@@ -41,14 +42,14 @@ var displayRepos = function(repos, searchTerm) {
         return;
     }
     console.log(repos);
-    console.log(searchTerm);
-    repoContainerEl.textContent = "";
+    console.log(searchTerm)
     repoSearchTerm.textContent = searchTerm;
 
         for (var i = 0; i < repos.length; i++) {
             var repoName = repos[i].owner.login + "/" + repos[i].name;
-            var repoEl = document.createElement("div");
+            var repoEl = document.createElement("a");
             repoEl.classList = "list-item flex-row justify-space-between align-center";
+            repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
             var titleEl = document.createElement("span");
             titleEl.textContent = repoName;
             repoEl.appendChild(titleEl);
--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -2,6 +2,20 @@ var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    console.log(repoName);
+
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        document.location.replace("./index.html");
+    }
+};
+
+
 var getRepoIssues = function(repo) {
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -17,31 +31,9 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            console.log(response);
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
-    });
-    // for (let i = 0; i < issues.length; i++) {
-    //     var issueEl = document.createElement("a");
-    //     issueEl.classList = "list-item flex-row justify-space-between align-center";
-    //     issueEl.setAttribute("href", issues[i].html_url);
-    //     issueEl.setAttribute("target", "_blank");
-        
-    //     var titleEl = document.createElement("span");
-    //     titleEl.textContent = issues[i].title;
-
-    //     issueEl.appendChild(titleEl);
-
-    //     var typeEl = document.createElement("span");
-
-    //     if (issues[i].pull_request) {
-    //         typeEl.textContent = "(Pull request)";
-    //     } else {
-    //         typeEl.textContent = "(Issue)"
-    //     }
-    //     issueEl.appendChild(typeEl)
-    //     issueContainerEl.appendChild(issueEl);
-    // }
+    }); 
 };
 
 var displayIssues = function(issues) {
@@ -78,8 +70,9 @@ var displayWarning = function(repo) {
     var linkEl = document.createElement("a");
     linkEl.textContent = "GitHub.com";
     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
 
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName()
